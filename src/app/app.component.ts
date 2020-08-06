b7a1a6fa5d537111d34d2e91aa8bb1ca076445c4
artreimagined <teri.t.davis@gmail.com>
2020-08-06 19:55:24 -0400

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'application';
+  title = 'Cartoon Yourself!';
+  description = 'If you have ever wondered what you would look like as your favorite cartoon, here is your chance.'
 }
