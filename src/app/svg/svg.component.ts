088ed219edd2db1814041f39fa08ab6709157a7a
artreimagined <teri.t.davis@gmail.com>
2020-08-06 20:14:57 -0400

    This is the last thing
    I have each button working to show each svg. Yah!

--- src/app/svg/svg.component.ts
+++ src/app/svg/svg.component.ts
@@ -9,10 +9,35 @@ import { CartoonService } from '../service/cartoon.service';
 })
 export class SvgComponent implements OnInit {
 	isCollapsed : boolean = true;
+  isCollapsed1 : boolean = true;
+  isCollapsed2 : boolean = true;
+  isCollapsed3 : boolean = true;
+  isCollapsed4 : boolean = true;
+  isCollapsed5 : boolean = true;
 	
 	toggleCollapse() {
-	    this.isCollapsed = !this.isCollapsed;
-	  }
+	  this.isCollapsed = !this.isCollapsed;
+	}
+
+  toggleCollapse1() {
+      this.isCollapsed1 = !this.isCollapsed1;
+  }
+
+  toggleCollapse2() {
+      this.isCollapsed2 = !this.isCollapsed2;
+  }
+
+  toggleCollapse3() {
+      this.isCollapsed3 = !this.isCollapsed3;
+  }
+
+  toggleCollapse4() {
+      this.isCollapsed4 = !this.isCollapsed4;
+  }
+
+  toggleCollapse5() {
+      this.isCollapsed5 = !this.isCollapsed5;
+  }
 
 	cartoons: Cartoon[];
 
